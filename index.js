d07ffb26d414c709f1349bf21cf6226898cb1496
higiniecito <cdramirezh@unal.edu.co>
2024-05-31 15:44:46 -0500

    Perfil endpoints OK

--- index.js
+++ index.js
@@ -11,6 +11,8 @@ let db = new sqlite3.Database("./db.sqlite", err => {
 	console.log("Connected to the SQLite database.");
 });
 
+app.use(express.json());
+
 app.get("/users", (req, res) => {
 	const sql = "SELECT * FROM Usuario";
 	db.all(sql, [], (err, rows) => {
@@ -38,6 +40,32 @@ app.get("/customer", (req, res) => {
 			throw err;
 		}
 		res.send(rows);
+	});
+});
+
+// Create a new Perfil
+// Update a Perfil
+app.put("/perfil/:id", (req, res) => {
+	const { nombreperfil, estado } = req.body;
+	const { id } = req.params;
+	const sql = "UPDATE Perfil SET nombreperfil = ?, estado = ? WHERE Idperfil = ?";
+	db.run(sql, [nombreperfil, estado, id], function (err) {
+		if (err) {
+			return res.status(400).json({ error: err.message });
+		}
+		return res.json({ changes: this.changes });
+	});
+});
+
+// Delete a Perfil
+app.delete("/perfil/:id", (req, res) => {
+	const { id } = req.params;
+	const sql = "DELETE FROM Perfil WHERE Idperfil = ?";
+	db.run(sql, id, function (err) {
+		if (err) {
+			return res.status(400).json({ error: err.message });
+		}
+		return res.json({ deleted: this.changes });
 	});
 });
 
